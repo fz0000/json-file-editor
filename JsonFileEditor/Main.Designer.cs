6816ce78561fa70031b25fc03fb86eb7b8632ef9
fangzheng <fz0000@sina.com>
2023-07-18 18:12:03 +0800

    init
    Signed-off-by: fangzheng <fz0000@sina.com>

---
+++ JsonFileEditor/Main.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace JsonFileEditor
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBoxValue = new System.Windows.Forms.TextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.OToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.SToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripTextBoxFolder = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripTextBoxExclude = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripContainer1.ContentPanel.SuspendLayout();
+            this.toolStripContainer1.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.splitContainer1);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(1006, 694);
+            this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.Size = new System.Drawing.Size(1006, 721);
+            this.toolStripContainer1.TabIndex = 0;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer1.TopToolStripPanel
+            // 
+            this.toolStripContainer1.TopToolStripPanel.Controls.Add(this.toolStrip1);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.dataGridView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.textBoxValue);
+            this.splitContainer1.Size = new System.Drawing.Size(1006, 694);
+            this.splitContainer1.SplitterDistance = 335;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(1006, 335);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEnter);
+            // 
+            // textBoxValue
+            // 
+            this.textBoxValue.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxValue.Location = new System.Drawing.Point(0, 0);
+            this.textBoxValue.Multiline = true;
+            this.textBoxValue.Name = "textBoxValue";
+            this.textBoxValue.ReadOnly = true;
+            this.textBoxValue.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxValue.Size = new System.Drawing.Size(1006, 355);
+            this.textBoxValue.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.OToolStripButton,
+            this.SToolStripButton,
+            this.toolStripSeparator,
+            this.toolStripLabel1,
+            this.toolStripTextBoxFolder,
+            this.toolStripSeparator1,
+            this.toolStripLabel2,
+            this.toolStripTextBoxExclude});
+            this.toolStrip1.Location = new System.Drawing.Point(4, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(805, 27);
+            this.toolStrip1.TabIndex = 0;
+            // 
+            // OToolStripButton
+            // 
+            this.OToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.OToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("OToolStripButton.Image")));
+            this.OToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.OToolStripButton.Name = "OToolStripButton";
+            this.OToolStripButton.Size = new System.Drawing.Size(29, 24);
+            this.OToolStripButton.Text = "打开(&O)";
+            this.OToolStripButton.Click += new System.EventHandler(this.OToolStripButton_Click);
+            // 
+            // SToolStripButton
+            // 
+            this.SToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.SToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("SToolStripButton.Image")));
+            this.SToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.SToolStripButton.Name = "SToolStripButton";
+            this.SToolStripButton.Size = new System.Drawing.Size(29, 24);
+            this.SToolStripButton.Text = "保存(&S)";
+            this.SToolStripButton.Click += new System.EventHandler(this.SToolStripButton_Click);
+            // 
+            // toolStripSeparator
+            // 
+            this.toolStripSeparator.Name = "toolStripSeparator";
+            this.toolStripSeparator.Size = new System.Drawing.Size(6, 27);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(60, 24);
+            this.toolStripLabel1.Text = "Folder:";
+            // 
+            // toolStripTextBoxFolder
+            // 
+            this.toolStripTextBoxFolder.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F);
+            this.toolStripTextBoxFolder.Name = "toolStripTextBoxFolder";
+            this.toolStripTextBoxFolder.Size = new System.Drawing.Size(450, 27);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 27);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(69, 24);
+            this.toolStripLabel2.Text = "Exclude:";
+            // 
+            // toolStripTextBoxExclude
+            // 
+            this.toolStripTextBoxExclude.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F);
+            this.toolStripTextBoxExclude.Name = "toolStripTextBoxExclude";
+            this.toolStripTextBoxExclude.Size = new System.Drawing.Size(100, 27);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1006, 721);
+            this.Controls.Add(this.toolStripContainer1);
+            this.MinimumSize = new System.Drawing.Size(1024, 600);
+            this.Name = "Main";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Json File Editor";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            this.toolStripContainer1.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton OToolStripButton;
+        private System.Windows.Forms.ToolStripButton SToolStripButton;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBoxFolder;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBoxExclude;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBoxValue;
+    }
+}
+
---
+++ JsonFileEditor/Main.cs
@@ -0,0 +1,146 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace JsonFileEditor
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            ReadConfig();
+        }
+        private void ReadConfig()
+        {
+            var registryKey = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(@"Software\JsonEditor");
+            if (registryKey != null)
+            {
+                var strPath = registryKey.GetValue("path");
+                if (strPath != null)
+                {
+                    toolStripTextBoxFolder.Text = strPath.ToString();
+                }
+                var strExclude = registryKey.GetValue("exclude");
+                if (strExclude != null)
+                {
+                    toolStripTextBoxExclude.Text = strExclude.ToString();
+                }
+            }
+        }
+
+        private void WriteConfig()
+        {
+            Microsoft.Win32.RegistryKey registryKey = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(@"Software\JsonEditor");
+            registryKey.SetValue("path", toolStripTextBoxFolder.Text);
+            registryKey.SetValue("exclude", toolStripTextBoxExclude.Text);
+            registryKey.Close();
+        }
+
+        private void OToolStripButton_Click(object sender, EventArgs e)
+        {
+            string strDataFolder = toolStripTextBoxFolder.Text;
+            if (strDataFolder == "")
+            {
+                strDataFolder = Directory.GetCurrentDirectory();
+            }
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.SelectedPath = strDataFolder;
+            DialogResult dgRes = folderBrowserDialog.ShowDialog();
+            if (dgRes == DialogResult.OK)
+            {
+                toolStripTextBoxFolder.Text = folderBrowserDialog.SelectedPath;
+                LoadDataFiles(folderBrowserDialog.SelectedPath);
+            }
+
+        }
+
+        private void SToolStripButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("还没做好");
+        }
+
+        private void LoadDataFiles(string strFolder)
+        {
+            dataGridView1.Columns.Clear();
+            int intKey = dataGridView1.Columns.Add("key", "key");
+            dataGridView1.Columns[intKey].Resizable = DataGridViewTriState.True;
+            dataGridView1.Columns[intKey].Frozen = true;
+
+            string[] jsonFiles = Directory.GetFiles(strFolder, "*.json");
+            Dictionary<string, int> dictKeys = new Dictionary<string, int>();//value, intValue
+            foreach (string file in jsonFiles)
+            {
+                if (!file.Contains(toolStripTextBoxExclude.Text))
+                {
+                    //col
+                    string strColName = Path.GetFileNameWithoutExtension(file);
+                    dataGridView1.Columns.Add(strColName, strColName);
+                    //row
+                    string strJSON = File.ReadAllText(file);
+                    JObject objJson = JObject.Parse(strJSON);
+
+                    //create table
+                    foreach (KeyValuePair<string, JToken> token in objJson)
+                    {
+                        //read data
+                        string key = token.Key;
+
+                        DataGridViewRow row;
+                        if (!dictKeys.ContainsKey(key))
+                        {
+                            row = new DataGridViewRow();
+                            DataGridViewTextBoxCell cellValue = new DataGridViewTextBoxCell();
+                            cellValue.Value = key;
+                            row.Cells.Add(cellValue);
+                            int intRow = dataGridView1.Rows.Add(row);
+                            dictKeys.Add(key, intRow);
+                        }
+                    }
+
+                    //write to table
+                    foreach (KeyValuePair<string, JToken> token in objJson)
+                    {
+                        //read data
+                        string key = token.Key;
+                        string value;
+                        if (token.Value == null) { value = ""; }
+                        else
+                        {
+                            if (token.Value.Type == JTokenType.Array)
+                            {
+                                value = string.Join("\r\n", token.Value);
+                            }
+                            else
+                            {
+                                value = token.Value.ToString();
+                            }
+                        }
+
+                        dataGridView1.Rows[dictKeys[key]].Cells[strColName].Value = value;
+                    }
+                }
+            }
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            WriteConfig();
+        }
+
+        private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            var value = dataGridView1.CurrentCell.Value;
+            if (value == null)
+            {
+                textBoxValue.Text = "";
+            }
+            else
+            {
+                textBoxValue.Text = dataGridView1.CurrentCell.Value.ToString();
+            }
+        }
+    }
+}
